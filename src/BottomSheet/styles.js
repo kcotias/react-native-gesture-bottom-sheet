c8ed717b0a739a4432074ff09ca9a0d93dcb4f1a
aidanarmstrong <aidan.armstrong@hallnet.co.uk>
2021-06-24 15:27:16 +0100

    Allows component to accept a background color for the animated view (Panel). updated Animate.event to allow config options removes warning from development.

--- src/BottomSheet/styles.js
+++ src/BottomSheet/styles.js
@@ -10,7 +10,6 @@ const styles = StyleSheet.create({
     backgroundColor: "transparent",
   },
   container: {
-    backgroundColor: "#F3F3F3",
     width: "100%",
     height: 0,
     overflow: "hidden",
